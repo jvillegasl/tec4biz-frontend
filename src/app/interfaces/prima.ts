7225279f0c5111c95b1ec61784a950a0e550b659
jvillegasl <jvillegasl@uni.pe>
2023-11-15 16:19:54 -0500

    feat: Add prima interface

---
+++ src/app/interfaces/prima.ts
@@ -0,0 +1,10 @@
+export interface Prima {
+	liquidacionPrima: string;
+	codigoProducto: string;
+	numeroPoliza: string;
+	codigoAsegurado: string;
+	tipoDocumento: string;
+	tipoRegistro: string;
+	numeroDocumento: string;
+	codigoCliente: string;
+}
